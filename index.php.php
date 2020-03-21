ce603e74b4cd605daa9746570923b9fb9073878f
Aq <aqcornwall@gmail.com>
2020-03-21 23:08:00 +0000

    htmlspecialchars_decode, nested for, nested if, substr_count for img src and wp-block-embed-youtube implementation

--- index.php.php
+++ index.php.php
@@ -2,42 +2,48 @@
 
 $string= '<!-- wp:image {"id":2685} --> <figure class="wp-block-image"><img src="https://cronache.test/wp-content/uploads/2020/02/fondazioneveronesi.jpg" alt="" class="wp-image-2685"/><figcaption>Ph Fondazione Veronesi</figcaption></figure> <!-- /wp:image --> <!-- wp:paragraph --> <p></p> <!-- /wp:paragraph --> <!-- wp:paragraph --> <p>A proposito di assenzio e di junk food, le colonie di benpensanti sulle due sponde dell\'Atlantico si spendono facilmente a favore delle droghe leggere. Un musicista americano, profondo conoscitore del jazz e che stimo, ha pubblicato la solita tiritera dell\'origine del veto alla cannabis, certamente in buona fede. Riportava correttamente che il bando fu deciso da interessi economici e aveva ragione: per quanto ne so io, la cannabis contrastava gli interessi di chi utilizzava la cellulosa di legno, proprio alla vigilia degli investimenti cospicui di William Randolph Hearst, che aveva acquistato foreste intere per farsi la carta. E del gruppo chimico Dupont. Vi sono capitate le pipette (talvolta in schiuma) da signora, espressamente destinate alle donne per lenire i loro fastidi dalle mestruazioni alla cefalea? La cannabis era prescritta dal medico.<br>Ma i tempi sono cambiati, e la cannabis non si utilizza più come fibra industriale. Oggi chi la compra nella stragrande maggioranza lo fa per sballare ed è nociva e pericolosa. Emerge quella che Pasolini tra gli altri definiva una cultura edonista, subdolamente associata al concetto rafforzativo di libertà e democrazia. No, nessuno dice che per affermare la tua libertà devi sballare e penso a ragione che la cannabis produca spesso una assuefazione sociale a tutte le droghe, anche le più pesanti. Personalmente dico che non è giusto moralmente e che le droghe dove e quando si usano richiedono una iniziazione rigorosa; il fine di quelle esperienze quando non è la sopravvivenza è la espansione della coscienza, tutto il contrario dello sballo.<br>Si fanno guerre per difendere il modello consumista. Inoltre il musicista americano definiva "razzista" la semplice, evidente constatazione che i musicisti (e poi tutto l\'ambiente del Rock) ne fanno un uso intenso. Non c\'è peggior sordo e cieco di chi non voglia arrendersi alla realtà. E mi trovo da solo contro un pollaio di contestatori in stile neo-hippie. Che c\'entra il razzismo?<br>Le droghe furono seguite dalla CIA in un programma molto complesso e gelosamente nascosto alla opinione pubblica per quanto mirasse alla loro diffusione e al controllo delle persone che ne facevano uso, M-K-ULTRA. Si commisero azioni criminali pur di sperimentarla, il più delle volte alla insaputa delle vittime. In un primo momento ci si concentrò sugli allucinogeni e fu la stagione dello LSD. Si lavorò a fondo nei campi universitari e nell\'ambiente studentesco in genere. Intellettuali e artisti entusiasti furono coinvolti e strumentalizzati. Fino ai nostri giorni e alle esperienze dell\'ISIS, che ricordano le gesta degli Assassini puntigliosamente. Cosa è cambiato? Le modalità degli attentati terroristici: gli attentatori sono condotti sul luogo dell\'attentato e dopo l\'impresa spesso abbattuti da agenti appostati a due passi. Sono in circolazione sostanze che ti rovinano la mente dopo una dose soltanto. Cosa ormai vecchia e risaputa (ma guarda!) la associazione tra droghe di tutti i tipi e il sesso.</p> <!-- /wp:paragraph -->';
 $string= '<!-- wp:paragraph --> <p>Università degli Studi di Bari: una prima analisi sulla diffusione del COVID-19 in Italia</p> <!-- /wp:paragraph --> <!-- wp:image {"id":2865} --> <figure class="wp-block-image"><img src="https://cronachemediterranee.org/wp-content/uploads/2020/03/pag1.jpg" alt="" class="wp-image-2865"/></figure> <!-- /wp:image --> <!-- wp:image {"id":2866} --> <figure class="wp-block-image"><img src="https://cronachemediterranee.org/wp-content/uploads/2020/03/pag2.jpg" alt="" class="wp-image-2866"/></figure> <!-- /wp:image --> <!-- wp:image {"id":2867} --> <figure class="wp-block-image"><img src="https://cronachemediterranee.org/wp-content/uploads/2020/03/pag3.jpg" alt="" class="wp-image-2867"/></figure> <!-- /wp:image --> <!-- wp:image {"id":2868} --> <figure class="wp-block-image"><img src="https://cronachemediterranee.org/wp-content/uploads/2020/03/pag4.jpg" alt="" class="wp-image-2868"/></figure> <!-- /wp:image --> <!-- wp:image {"id":2869} --> <figure class="wp-block-image"><img src="https://cronachemediterranee.org/wp-content/uploads/2020/03/pag5.jpg" alt="" class="wp-image-2869"/></figure> <!-- /wp:image --> <!-- wp:image {"id":2870} --> <figure class="wp-block-image"><img src="https://cronachemediterranee.org/wp-content/uploads/2020/03/pag6.jpg" alt="" class="wp-image-2870"/></figure> <!-- /wp:image -->';
+$string= '<!-- wp:image {"id":2649} --> <figure class="wp-block-image"><img src="https://cronache.test/wp-content/uploads/2020/02/1controcopertina.jpg" alt="" class="wp-image-2649"/></figure> <!-- /wp:image --> <!-- wp:paragraph --> <p></p> <!-- /wp:paragraph --> <!-- wp:paragraph --> <p>Il 2 febbraio di ogni anno la città di Catania fa festa con le Candelore di Sant’Agata.<br>Quaranta giorni dopo il Natale – il 2 febbraio, per l’appunto - ricorre la Festa della purificazione di Maria Vergine, nota anche con il nome di “Candelora”. Le norme della purificazione per la donna che partoriva un maschio si leggono nel Levitico 12,2-4; lo stesso passo dice che nel caso di Maria la purificazione coincide con la presentazione del Bambino Gesù al Tempio. Recita il Levitico 12,2-4: “Parla così ai figli d\'Israele: Quando una donna sarà rimasta incinta e partorirà un maschio, sarà impura per sette giorni; sarà impura come nei giorni del suo ciclo mestruale. L\'ottavo giorno il bambino sarà circonciso. La donna poi resterà ancora trentatré giorni a purificarsi del suo sangue; non toccherà nessuna cosa santa e non entrerà nel santuario finché non siano compiuti i giorni della sua purificazione”.<br>La Candelora ci ricorda che la “Presentazione del Signore” è “la festa di Cristo luce delle genti e dell\'incontro del Messia con il suo popolo nel Tempio di Gerusalemme”. <br>Buona “luce”, buona festa di Sant’Agata alla città di Catania.</p> <!-- /wp:paragraph --> <!-- wp:paragraph --> <p><em>Ph Cronache Mediterranee,&nbsp;"Candelore di Sant\'Agata", Catania</em></p> <!-- /wp:paragraph --> <!-- wp:paragraph --> <p></p> <!-- /wp:paragraph --> <!-- wp:image {"id":2650} --> <figure class="wp-block-image"><img src="https://cronache.test/wp-content/uploads/2020/02/2.jpg" alt="" class="wp-image-2650"/></figure> <!-- /wp:image --> <!-- wp:image {"id":2651} --> <figure class="wp-block-image"><img src="https://cronache.test/wp-content/uploads/2020/02/3.jpg" alt="" class="wp-image-2651"/></figure> <!-- /wp:image --> <!-- wp:image {"id":2652} --> <figure class="wp-block-image"><img src="https://cronache.test/wp-content/uploads/2020/02/5.jpg" alt="" class="wp-image-2652"/></figure> <!-- /wp:image --> <!-- wp:image {"id":2653} --> <figure class="wp-block-image"><img src="https://cronache.test/wp-content/uploads/2020/02/6.jpg" alt="" class="wp-image-2653"/></figure> <!-- /wp:image -->';
+$string = '228<!-- wp:image {"id":2852} --> <figure class="wp-block-image"><img src="https://cronachemediterranee.org/wp-content/uploads/2020/03/aquila-arpia.jpg" alt="" class="wp-image-2852"/></figure> <!-- /wp:image --> <!-- wp:paragraph --> <p><em>Coronavirus e teorie del complotto</em><br>Dobbiamo fare lo sforzo necessario per uscire dai clichè del politicamente corretto, in modo da liberarci delle molte catene invisibili che ci legano a principi non dimostrati ma instillati dai mass media come imperativi categorici. Già, un lavoro che ha richiesto generazioni e la piena complicità dei mass media: CONTRO la democrazia, per favorire le minoranze al potere come élite disposte strategicamente in modo da organizzare un presidio permanente. Il trucco delle mode. Non diamo mai niente per scontato e andiamo sempre a fondo: garantisco che quei mascalzoni sono in affanno appena facciamo sul serio. Di fronte alla verità soltanto pochi hanno il coraggio di accettare l\'errore, i più si abbandonano a un delirio psicotico. La sedicente sinistra incarna la catastrofe con una deriva assoluta, che è politica, fisica, morale e mentale. Mi convinco che i socialdemocratici nella società moderna hanno preso il posto dei Farisei, dimostrando di essere i peggiori.</p> <!-- /wp:paragraph --> <!-- wp:paragraph --> <p><br>Vi invito ad ascoltare il messaggio del filosofo Diego Fusaro e, a seguire, l\'opinione del dottore Stefano Montanari, perché esprimono due diverse ipotesi di intrigo a danno di persone.</p> <!-- /wp:paragraph --> <!-- wp:core-embed/youtube {"url":"https://youtu.be/CN9khnoNzCg","type":"video","providerNameSlug":"youtube","className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} --> <figure class="wp-block-embed-youtube wp-block-embed is-type-video is-provider-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper"> https://youtu.be/CN9khnoNzCg </div></figure> <!-- /wp:core-embed/youtube --> <!-- wp:paragraph --> <p></p> <!-- /wp:paragraph --> <!-- wp:core-embed/youtube {"url":"https://youtu.be/7AGVIe53WHc","type":"video","providerNameSlug":"youtube","className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} --> <figure class="wp-block-embed-youtube wp-block-embed is-type-video is-provider-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper"> https://youtu.be/7AGVIe53WHc </div></figure> <!-- /wp:core-embed/youtube -->';
 
 
 function countPostChar( $content ){
 
-	$string = htmlspecialchars($content);
+	$string = htmlspecialchars_decode($content);
 	$count='';
 	$paragraph='';
 	$string_test=$string;
 	$string = str_split($string);
-	foreach ( $string as $key => $value ) {
-		
-		// echo $string[$key+36];
-		// echo '<br>';
-		// echo print_r($string);
-		// echo $string_test;
+	$counter = count($string);
+	for ($i=0; $i < $counter ; $i++) { 
+		// echo htmlspecialchars_decode($string[$i+1]);
 		// die;
-		$paragraph.= $string[$key];
-
 		
-		if( array_key_exists($key+1, $string) &&
-			array_key_exists($key+2, $string) &&
-			array_key_exists($key+3, $string) &&
-			array_key_exists($key+4, $string) &&
-			$string[$key] == ';' &&  $string[$key+1] == 'p' ){
-				while ( $string[$key] <> '&' &&
-						$string[$key] <> '1' &&
-						$string[$key] <> &&
-						$string[$key] <> &&
-						$string[$key] <> &&
-				) {
-					# code...
+		if( array_key_exists($i+1, $string) && 
+			array_key_exists($i+2, $string) && 
+			array_key_exists($i+3, $string) &&
+			array_key_exists($i+4, $string) &&
+			htmlspecialchars_decode($string[$i]) == '<' &&
+			htmlspecialchars_decode($string[$i+1]) == 'p' &&
+			htmlspecialchars_decode($string[$i+2]) == '>'
+
+		){
+			$i= $i+3;
+			for ($j=$i; $j < $counter  ; $j++) { 
+				if (htmlspecialchars_decode($string[$i]) == '<' && htmlspecialchars_decode($string[$i+1]) == '/'){
+					$j=0;
+					break;
 				}
-		}
-	}
-		// echo json_encode($paragraph);
-		return strlen($paragraph);
+				$paragraph.= htmlspecialchars_decode($string[$i]);
+				$i++;
+
+			}//nested for
+
+		}// if
+
+	}//for
+	$extra_char = substr_count($content, "img src")*1000;
+	
+	return strlen($paragraph) + $extra_char;
 }
 
 echo countPostChar( $string );
